bb32efa18919bbf58f2603e6f8276a9866614113
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2020-12-24 20:10:21 -0800

    dp: array hooper in linear time

---
+++ src/dynamicProgramming/Q7_ArrayHooper.java
@@ -0,0 +1,55 @@
+package dynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Q. find min jump path. 
+ * 
+ * e.g. 
+ * Input: nums = [2,3,1,1,4]
+ * Output: [0, 1]
+ * 
+ * leetcode) 
+ * jump game2: find number of min jump
+ * https://leetcode.com/problems/jump-game-ii/solution/
+ * @author sunnypark
+ * 
+ */
+public class Q7_ArrayHooper {
+	public static List<Integer> findMinJumpPath(int[] arr) {
+		List<Integer> path = new ArrayList<>();
+		int currMax = 0; // reachable index in current jump
+		int possibleMax = 0; // possibly reachable index with max jump from current index 
+		int possibleMaxIdx = 0; // index for possible max jump
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			// 1) if index passes the curr max
+			if (currMax < i) {
+				 // edge case: [2, 0, 0, 0]
+				if (possibleMax == currMax) {
+					return List.of();
+				}
+				path.add(possibleMaxIdx);
+				currMax = possibleMax;
+			}
+
+			// 2) if possible max needs update
+			// which means jump from curr index should be picked
+			if (i + arr[i] > possibleMax) {
+				possibleMax = i + arr[i];
+				possibleMaxIdx = i;
+			}
+		}
+
+		return path;
+    }
+    
+    public static void main(String[] args) {
+        int[] input1 = {2,3,1,1,2,4,2,0,1,1};
+        int[] input2 = {2,0,0,0};
+        System.out.println(findMinJumpPath(input1));
+        System.out.println(findMinJumpPath(input2));
+    }
+}
