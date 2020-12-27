94deeb2ca727be2f859689bb4f6cc3f18c6ac7bc
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2020-12-26 23:58:43 -0800

    binary search

---
+++ src/binarySearch/Type1_1SearchInFloatArray.java
@@ -0,0 +1,19 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class Type1_1SearchInFloatArray {
+	public static int binarySearchInFloatArray(float[] arr, int t) {
+		int pos = Arrays.binarySearch(arr, (float) t);
+		if (pos < 0) {
+			pos = Math.abs(pos + 1);
+		}
+		
+		return Math.abs(t - arr[pos - 1]) < Math.abs(t - arr[pos]) ? pos - 1 : pos;
+	}
+	
+	public static void main(String[] args) {
+		float[] input = {1.2f, 2.3f, 3.3f};
+		System.out.println(binarySearchInFloatArray(input, 2));
+	}
+}
