d368add5208d16a836d525c59f068de2529b4e0b
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2021-01-04 23:34:00 -0800

    lc 1438

---
+++ src/practice/LC1438_LongestSubarrayWithAbsDiffLimit.java
@@ -0,0 +1,18 @@
+package practice;
+
+/**
+ * LC 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+ * Q Given an array of integers nums and an integer limit, 
+ * return the size of the longest non-empty subarray 
+ * such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+ * 
+ * e.g. 
+ * Input: nums = [8,2,4,7], limit = 4 
+ * Output: 2 
+ * 
+ * @author sunnypark
+ *
+ */
+public class LC1438_LongestSubarrayWithAbsDiffLimit {
+
+}
