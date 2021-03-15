f272d8ac4a663d03dde2fe191d3a5e6b22c3467f
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2021-03-14 23:57:37 -0700

    sliding window

---
+++ src/grokking/slidingWindow/AverageOfSubarrays.java
@@ -0,0 +1,10 @@
+package grokking.slidingWindow;
+
+/**
+ * Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+ * @author sunnypark
+ *
+ */
+public class AverageOfSubarrays {
+
+}
